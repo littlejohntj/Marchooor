6db32118bb516b2d3c592105a65ce87d9f062643
littlejohntj <tjl13c@my.fsu.edu>
2023-10-02 02:45:40 -0400

    Initial Commit

---
+++ Marchoooor/MarchoooorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarchoooorApp.swift
+//  Marchoooor
+//
+//  Created by Todd Littlejohn on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MarchoooorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
